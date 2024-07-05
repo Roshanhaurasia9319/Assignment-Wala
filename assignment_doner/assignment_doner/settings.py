875fc5c11a9146566e98224f7b4ea3b427316eef
Roshan lal chaurasia <Roshanlalchaurasia9319@gmail.com>
2024-07-05 10:38:10 +0530

    f

--- assignment_doner/assignment_doner/settings.py
+++ assignment_doner/assignment_doner/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'assignment_doner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'assignment_wala',
+        'USER': 'root',
+        'PASSWORD': 'Roshan@1234',
+        'HOST': 'localhost',   
+        'PORT': '3306',        
     }
 }
 
